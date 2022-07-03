3f1ecd78ae4d16260f8fd27877846a83f0f2ef12
«Alicia» <alicia@ringover.com>
2022-07-03 21:33:00 +0200

    link problems e

--- eapCSV.go
+++ eapCSV.go
@@ -73,6 +73,7 @@ func FactstoCSV(content []*OrderCSV, etabid int64, start string, end string) (li
 
 	var rows [][]string
 
+	fmt.Println("123")
 	filename := strconv.FormatInt(etabid, 10) + "_" + strings.ReplaceAll(start, " ", "-") + "_to_" + strings.ReplaceAll(end, " ", "-") + "-export.csv"
 	// filepath := viper.GetString("links.cdn_csv_dest") + filename
 	filepath := "/home/ec2-user/media/csv/" + filename
